e4855cd76c5ef49975d859e3c49abf872f7854d9
TRANDANGTIEN99 <trandangtien10b1@gmail.com>
2024-07-01 14:26:17 +0900

    .

--- JavaProject.md/src/D5/probs/D52Recursive.java
+++ JavaProject.md/src/D5/probs/D52Recursive.java
@@ -1,28 +1,45 @@
 package D5.probs;
-
+//// 재귀 함수 문제
 public class D52Recursive {
     public static void main(String[] args) {
-        System.out.println(factRecur(1));
+        System.out.println(factLoop(5));
+        System.out.println(factLoop(6));
+        System.out.println(factLoop(10));
         System.out.println(factRecur(5));
+        System.out.println(factRecur(6));
         System.out.println(factRecur(10));
-
-
+        System.out.println(fibo(12));
     }
-    public static int factLoop(int n){
+
+    // 방법1: 반복문을 사용한다.
+    public static int factLoop(int n) {
         int result = 1;
-        for (int i = 1; i <  n ; i++) {
-            result *= 1;
-
+        // i는 1 ~ n 까지 반복해서 커진다.
+        for (int i = 1; i < n + 1; i++) {
+            result *= i;
         }
+        return result;
+    }
 
-    return result;
+    // 방법2: f(n) = n * f(n - 1)을 코드로 옮긴다.
+    // f(1) == 1, f(0) == 1, 0! = 1
+    public static int factRecur(int n) {
+//        if (n < 2) {
+//            return 1;
+//        }
+//        else {
+//            // factRecur 내부에서 factRecur를 호출한다.
+//            // 재귀적 호출 -> 재귀 함수
+//            int result = n * factRecur(n - 1);
+//            return result;
+//        }
+        if (n < 2) return 1;
+        return n * factRecur(n - 1);
+    }
 
+    public static int fibo(int n) {
+        if (n == 0) return 0;
+        if (n < 3) return 1;
+        return fibo(n - 2) + fibo(n - 1);
+    }
 }
-public static int factRecur(int n){
-        if (n < 2){
-            return 1;
-        }
-        else{ int result = n + factLoop( n-1);
-            return 1;
-}
-}}
